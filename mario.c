b4cf712d045439ec47afc8267c0c9dd48674e7e1
Addd12 <Addd12@users.noreply.github.com>
2019-09-09 22:34:41 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -9,19 +9,19 @@ int main(void)
         //prompts the user to enter the height
         n = get_int("Please enter the number of blocks: ");
     } 
-    while (n<1 || n>8); //check if the input is valid
+    while (n < 1 || n > 8); //check if the input is valid
     
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-       for (int j=n-i; j>1; j--)
-       {
+        for (int j = n-i; j > 1; j--)
+        {
            printf(" "); //prints spaces until the condition is met   
-       }
+        }
        
-       for (int j = 0; j < i+1; j++)
-       {
+        for (int j = 0; j < i+1; j++)
+        {
            printf("#"); //prints hashes until the condition is met   
-       }
+        }
         printf("\n");
     }
 }
